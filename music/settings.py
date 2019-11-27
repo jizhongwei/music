4e38dd3f57f7c92caa4963fcde18fe5992652794
dick <jzwxiaoniao@163.com>
2019-11-27 09:25:09 +0800

    项目基本配置完成

--- music/settings.py
+++ music/settings.py
@@ -37,11 +37,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'index',
+    'ranking',
+    'user',
+    'play',
+    'search',
+    'comment',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
